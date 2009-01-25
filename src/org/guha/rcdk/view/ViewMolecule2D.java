bdad804e32f6f855c0075df36065e7957c936634
rajarshi (Rajarshi Guha) <rajarshi.guha@gmail.com>
2009-01-25 22:35:20 +0000

    Updated code to use the latest renderer
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcdk@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/org/guha/rcdk/view/ViewMolecule2D.java
+++ src/org/guha/rcdk/view/ViewMolecule2D.java
@@ -1,29 +1,22 @@
 package org.guha.rcdk.view;
 
 import org.guha.rcdk.util.Misc;
+import org.guha.rcdk.view.panels.Render2DPanel;
 import org.openscience.cdk.aromaticity.CDKHueckelAromaticityDetector;
 import org.openscience.cdk.exception.CDKException;
-import org.openscience.cdk.geometry.GeometryTools;
 import org.openscience.cdk.graph.ConnectivityChecker;
 import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.interfaces.IMolecule;
-import org.openscience.cdk.layout.StructureDiagramGenerator;
-import org.openscience.cdk.renderer.Java2DRenderer;
-import org.openscience.cdk.renderer.Renderer2DModel;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 
-public class ViewMolecule2D extends JPanel {
+public class ViewMolecule2D extends JFrame {
     IAtomContainer molecule;
-    JFrame frame;
 
-    Renderer2DModel r2dm;
-    Java2DRenderer renderer;
+    Render2DPanel panel;
 
     int width = 300;
     int height = 300;
@@ -31,71 +24,33 @@ public class ViewMolecule2D extends JPanel {
 
     class ApplicationCloser extends WindowAdapter {
         public void windowClosing(WindowEvent e) {
-            frame.dispose();
+            dispose();
         }
     }
 
-    public ViewMolecule2D(IAtomContainer molecule) throws CDKException {
-        this.molecule = molecule;
-
+    public ViewMolecule2D(IAtomContainer molecule) throws Exception {
         if (!ConnectivityChecker.isConnected(molecule)) throw new CDKException("Molecule must be connected");
-
-        frame = new JFrame("2D Structure Viewer");
-        frame.addWindowListener(new ApplicationCloser());
-        frame.setSize(300, 300);
-
-    }
-
-    public void paint(Graphics g) {
-        super.paint(g);
-        renderer.paintMolecule(molecule, (Graphics2D) g, getBounds());
-    }
-
-    public void draw() {
         molecule = AtomContainerManipulator.removeHydrogens(molecule);
         try {
             CDKHueckelAromaticityDetector.detectAromaticity(molecule);
         } catch (CDKException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            throw new Exception("Error in aromatcity detection");
         }
-
-
-        r2dm = new Renderer2DModel();
-        renderer = new Java2DRenderer(r2dm);
-        Dimension screenSize = new Dimension(this.width, this.height);
-        setPreferredSize(screenSize);
-        r2dm.setBackgroundDimension(screenSize); // make sure it is synched with the JPanel size
-        setBackground(r2dm.getBackColor());
-
-        try {
-            StructureDiagramGenerator sdg = new StructureDiagramGenerator();
-            sdg.setMolecule((IMolecule) molecule);
-            sdg.generateCoordinates();
-            molecule = sdg.getMolecule();
-
-            r2dm.setDrawNumbers(false);
-            r2dm.setUseAntiAliasing(true);
-            r2dm.setColorAtomsByType(true);
-            r2dm.setShowImplicitHydrogens(true);
-            r2dm.setShowAromaticity(true);
-            r2dm.setShowReactionBoxes(false);
-            r2dm.setKekuleStructure(false);
-
-            GeometryTools.translateAllPositive(molecule);
-            GeometryTools.scaleMolecule(molecule, getPreferredSize(), this.scale);
-            GeometryTools.center(molecule, getPreferredSize());
-
-        }
-        catch (Exception exc) {
-            exc.printStackTrace();
-        }
-        frame.getContentPane().add(this);
-        frame.pack();
-        frame.setVisible(true);
+        molecule = Misc.getMoleculeWithCoordinates(molecule);
+        panel = new Render2DPanel(molecule, null, 200, 200, false);
+        setTitle("2D Viewer");
+        addWindowListener(new ApplicationCloser());
+        setSize(300, 300);
     }
 
-    public static void main(String[] arg) throws CDKException {
-        String home = "/home/rguha/";
+    public void draw() {
+        getContentPane().add(panel);
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] arg) throws Exception {
+        String home = "/Users/rguha/";
         String[] fname = {home + "src/R/trunk/rcdk/data/dan001.sdf",
                 home + "src/R/trunk/rcdk/data/dan002.sdf",
                 home + "src/R/trunk/rcdk/data/dan003.sdf"};
